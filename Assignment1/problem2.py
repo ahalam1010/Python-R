85dba5f0d34a0bbd7e7219ed118c6dc92986c7c5
ahalam1010 <ahalamabdulhakim@gmail.com>
2026-07-09 08:25:23 +0600

    Add  problem 2

---
+++ Assignment1/problem2.py
@@ -0,0 +1,8 @@
+numbers = []
+
+for i in range(10):
+    value = int(input("Enter an integer: "))
+    numbers.append(value)
+
+print("The complete list is:", numbers)
+print("Total number of elements:", len(numbers))
