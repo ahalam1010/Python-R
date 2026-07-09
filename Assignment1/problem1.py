ed79aa9c58f54c7e23432958d4ef797a38077dbf
ahalam1010 <ahalamabdulhakim@gmail.com>
2026-07-09 08:21:23 +0600

    Add problem 1

---
+++ Assignment1/problem1.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+print("The complete list is:", numbers)
+print("The first element is:", numbers[0])
+print("The last element is:", numbers[-1])
+print("The length of the list is:", len(numbers))
